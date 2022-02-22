73e937b644182937e73c67414c0554bce1ec8598
keertirajmalik <keertirajmalik@gmail.com>
2022-02-22 10:18:07 +0530

    Adding password encoder and adding log

--- src/main/java/com/codingmonkey/studentmanagement/StudentManagementApplication.java
+++ src/main/java/com/codingmonkey/studentmanagement/StudentManagementApplication.java
@@ -1,12 +1,17 @@
 package com.codingmonkey.studentmanagement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementApplication {
 
-  public static void main(String[] args) {
+  private static final Logger logger = LoggerFactory.getLogger(StudentManagementApplication.class);
+
+  public static void main(String... args) {
+    logger.info("Started Application with configurations :: {}", args);
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 

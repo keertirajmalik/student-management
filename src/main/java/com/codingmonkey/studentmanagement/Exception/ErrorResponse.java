a9e77fd74a1bf34f24042914739fc7d30dae66d8
keertirajmalik <keertirajmalik@gmail.com>
2022-02-22 15:52:05 +0530

    Created a Exception handler

---
+++ src/main/java/com/codingmonkey/studentmanagement/Exception/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.codingmonkey.studentmanagement.Exception;
+
+public class ErrorResponse {
+
+  private int code;
+  private String message;
+  private String timeStamp;
+
+  public ErrorResponse() {
+  }
+
+  public ErrorResponse(final int code, final String message, final String timeStamp) {
+    this.code = code;
+    this.message = message;
+    this.timeStamp = timeStamp;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public void setCode(final int code) {
+    this.code = code;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(final String message) {
+    this.message = message;
+  }
+
+  public String getTimeStamp() {
+    return timeStamp;
+  }
+
+  public void setTimeStamp(final String timeStamp) {
+    this.timeStamp = timeStamp;
+  }
+}

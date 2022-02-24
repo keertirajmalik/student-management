e43ab8e1358c4f64925047a172b26ec40de67397
keertirajmalik <keertirajmalik@gmail.com>
2022-02-24 15:51:48 +0530

    SQL file to load some predefined data

---
+++ src/main/resources/db/changelog/v1/scripts/ADD SUBJECTS.sql
@@ -0,0 +1,4 @@
+INSERT INTO student_data.subject_details (subject_id, subject, class_number)
+VALUES (5, 'Kannada', 10);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number)
+VALUES (6, 'English', 10);

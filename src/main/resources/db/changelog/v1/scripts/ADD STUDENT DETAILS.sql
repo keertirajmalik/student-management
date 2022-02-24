cb99805db8573f42c8e26ee94b6475d1a76bcef3
Coding Monkey <38658113+keertirajmalik@users.noreply.github.com>
2022-02-24 15:54:07 +0530

    Add liquibase to project (#11)
    * Added the maven dependency for the liquibase
    
    * SQL file to load some predefined data
    
    * Setup files for the liquibase

---
+++ src/main/resources/db/changelog/v1/scripts/ADD STUDENT DETAILS.sql
@@ -0,0 +1,3 @@
+INSERT INTO student_data.student_details (student_id, first_name, last_name, roll_number, mobile_number, email,
+                                          class_number)
+VALUES (1, 'keerti', 'malik', 1, 8277272285, 'keerti@icloud.com', 10);

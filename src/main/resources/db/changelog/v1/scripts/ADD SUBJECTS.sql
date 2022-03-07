d405667d87ce5c764a5bcc48da9a8387bbae74ed
keertirajmalik <keertirajmalik@gmail.com>
2022-03-07 18:14:23 +0530

    Added few more entry to the Subject Table

--- src/main/resources/db/changelog/v1/scripts/ADD SUBJECTS.sql
+++ src/main/resources/db/changelog/v1/scripts/ADD SUBJECTS.sql
@@ -2,3 +2,17 @@ INSERT INTO student_data.subject_details (subject_id, subject, class_number, tea
 VALUES (1, 'Kannada', 10, 1);
 INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
 VALUES (2, 'English', 10, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (3, 'Science', 10, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (4, 'Science', 9, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (5, 'Kannada', 9, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (6, 'English', 9, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (7, 'Science', 8, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (8, 'Kannada', 8, 1);
+INSERT INTO student_data.subject_details (subject_id, subject, class_number, teacher_id)
+VALUES (9, 'English', 8, 1);

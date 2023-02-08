e0736ef68367b3e507a48b872c59cd478f8678d4
Keertiraj Malik <keertirajlaxman.malik@imtf.com>
2023-02-08 10:19:28 +0530

    Removed unused methods from NotFoundException class

--- src/main/java/com/codingmonkey/studentmanagement/exception/NotFoundException.java
+++ src/main/java/com/codingmonkey/studentmanagement/exception/NotFoundException.java
@@ -1,25 +1,8 @@
 package com.codingmonkey.studentmanagement.exception;
 
 public class NotFoundException extends RuntimeException {
-  public NotFoundException() {
-  }
 
   public NotFoundException(final String message) {
     super(message);
-  }
-
-  public NotFoundException(final String message, final Throwable cause) {
-    super(message, cause);
-  }
-
-  public NotFoundException(final Throwable cause) {
-    super(cause);
-  }
-
-  public NotFoundException(final String message,
-                           final Throwable cause,
-                           final boolean enableSuppression,
-                           final boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
   }
 }

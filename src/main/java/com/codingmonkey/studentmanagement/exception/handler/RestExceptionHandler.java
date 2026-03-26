80085f7e73c5c6dfc792829e5856fe3b1afff5d8
keertiraj <keertirajmalik@gmail.com>
2026-03-26 21:43:39 +0530

    fix: refactor handleBadRequestException in RestExceptionHandler to accept generic RuntimeException

--- src/main/java/com/codingmonkey/studentmanagement/exception/handler/RestExceptionHandler.java
+++ src/main/java/com/codingmonkey/studentmanagement/exception/handler/RestExceptionHandler.java
@@ -73,21 +73,21 @@ public class RestExceptionHandler {
   }
 
   @ExceptionHandler
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<ErrorResponse> handleBadRequestException(StudentDetailsException exception) {
-    LOGGER.error("Error message :: {}", exception.getMessage());
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getLocalizedMessage(),
-                    Instant.now().toString()));
-  }
-
-  @ExceptionHandler
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ErrorResponse> handleException(NotFoundException exception) {
     LOGGER.error("Error message :: {}", exception.getMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
         .body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage(), Instant.now().toString()));
+  }
+
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public <T extends RuntimeException> ResponseEntity<ErrorResponse> handleBadRequestException(T exception) {
+    LOGGER.error("Error message :: {}", exception.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getLocalizedMessage(),
+                    Instant.now().toString()));
   }
 }
